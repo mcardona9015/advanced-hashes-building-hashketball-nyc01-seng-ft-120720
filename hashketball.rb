6956a31403fe0aec7a07264321f8891f9973ac74
Michael Cardona <mcardona9015@gmail.com>
2020-11-29 05:49:52 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -1,1 +1,38 @@
-# Write your code here!
+require 'pry'
+
+def game_hash
+  {
+    :home => {
+      :team_name => "",
+      :colors => ["", ""],
+      :players => [
+        {
+          :player_name => ,
+          :number => ,
+          :shoe => ,
+          :points => ,
+          :rebounds => ,
+          :assists => ,
+          :steals => ,
+          :blocks => ,
+          :slam_dunks => 
+        },
+        {
+          :player_name => ,
+          :number => ,
+          :shoe => ,
+          :points => ,
+          :rebounds => ,
+          :assists => ,
+          :steals => ,
+          :blocks => ,
+          :slam_dunks => 
+        },
+      ]
+    },
+    :away => {
+      
+    }
+  }
+  
+end
