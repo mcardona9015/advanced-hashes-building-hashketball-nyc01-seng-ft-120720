9436f627e3e7cfc4c302403c01ee0285e0dd2e65
Michael Cardona <mcardona9015@gmail.com>
2020-11-29 06:07:39 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -112,15 +112,15 @@ def game_hash
           :slam_dunks => 0
         },
         {
-          :player_name => 'Bismack Biyombo',
-          :number => 0,
-          :shoe => 16,
-          :points => 12,
-          :rebounds => 4,
-          :assists => 7,
-          :steals => 22,
-          :blocks => 15,
-          :slam_dunks => 10
+          :player_name => 'Kemba Walker',
+          :number => 33,
+          :shoe => 15,
+          :points => 6,
+          :rebounds => 12,
+          :assists => 12,
+          :steals => 7,
+          :blocks => 5,
+          :slam_dunks => 12
         }
       ]
     }
