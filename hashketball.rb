62f6f2b922e7767237eac114bfe3bf8ebda1c7a6
Michael Cardona <mcardona9015@gmail.com>
2020-11-29 05:56:17 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -3,9 +3,42 @@ require 'pry'
 def game_hash
   {
     :home => {
-      :team_name => "",
-      :colors => ["", ""],
+      :team_name => "Brooklyn Nets",
+      :colors => ["Black", "White"],
       :players => [
+        {
+          :player_name => '',
+          :number => '',
+          :shoe => '',
+          :points => '',
+          :rebounds => '',
+          :assists => '',
+          :steals => '',
+          :blocks => '',
+          :slam_dunks => ''
+        },
+        {
+          :player_name => '',
+          :number => '',
+          :shoe => '',
+          :points => '',
+          :rebounds => '',
+          :assists => '',
+          :steals => '',
+          :blocks => '',
+          :slam_dunks => ''
+        },
+        {
+          :player_name => '',
+          :number => '',
+          :shoe => '',
+          :points => '',
+          :rebounds => '',
+          :assists => '',
+          :steals => '',
+          :blocks => '',
+          :slam_dunks => ''
+        },
         {
           :player_name => '',
           :number => '',
